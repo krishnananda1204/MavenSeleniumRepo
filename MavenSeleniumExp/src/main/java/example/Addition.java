4617538dbfa78bdb91a53fb83579816f3c755a42
krishnananda1204 <krishnananda355@gmil.com>
2025-04-08 01:29:36 +0530

    Maven selenium project commited

---
+++ MavenSeleniumExp/src/main/java/example/Addition.java
@@ -0,0 +1,15 @@
+package example;
+
+public class Addition {
+
+	static int a=10;
+    static int b=20;
+    static int sum=a+b;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+		System.out.println(sum);
+	}
+
+}
---
+++ MavenSeleniumExp/src/main/java/example/Display.java
@@ -0,0 +1,17 @@
+package example;
+
+public class Display {
+    public void print()
+    {
+    	String s="Krishna";
+    	String b=" Nanda";
+    	String sum=s+b;
+    	System.out.println("Name: "+sum);
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Display obj=new Display();
+      obj.print();
+	}
+
+}
